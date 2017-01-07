c5fd54b7df1583f2ec56ba4c8641258fb245820b
Cristiano Ribeiro <christiribeiro2011@gmail.com>
2017-01-07 02:05:13 -0200

    Eventos javascript

--- js.js
+++ js.js
@@ -1,6 +1,32 @@
+/*var a = 2,
+    b = 3,
+    soma = a + b;
 
-var txt = "Hello!";
-var nome = "Name";
-var hora = "Good night"
 
-console.log(txt + " " + nome + " " + hora);
+function somar(){
+   return alert(a + b);
+}*/
+
+/*
+var uri = "http://christiweb.com.br/Ísso é JavaScript - Aprendizagem",
+
+   urlCodificada = encodeURI(uri);
+   urlDecodificada = decodeURI(uri);
+   urlCodificadaComponente = encodeURIComponent(uri);
+   urlDecodificadaComponente = decodeURIComponent(uri);
+
+
+   console.log(uri + "\n" + urlCodificada + "\n" + urlDecodificada + 
+      "\n" + urlCodificadaComponente + "\n" + urlDecodificadaComponente);
+
+*/
+
+
+console.log(isFinite(10));
+console.log(isFinite("10"));
+console.log(isFinite("jdsi"));
+
+
+var botao = document.elementById("botao");
+
+
